264295feee6a95238ddbad023b318fdc618ded34
sergey <110@mobilon.ru>
2024-03-29 15:26:19 +0700

    init

---
+++ config/default.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: {
+        server: {
+            port: 3000,
+            baseUrl: 'https://test-amo-widget-server.loca.lt',
+        },
+    },
+    amoApp: {
+        clientId: '7db5dbc1-bc1f-4166-9ab3-89a94ff38987',
+        clientSecret: 'ani6FwYiAw7R8cHsS0DNdLhkenbmx406A6djkUfyOmSfWqClcE4LmmOcLqYBzzz',
+    },
+    storeFile: './store/authdata.json',
+};
+
